49ce9115ecf1db7cdd64190582164ae54f2902ae
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-06-14 01:08:52 -0500

    + Main activity button
    * Added button which toggles between play and stop, it is supposed to display the state of the stream

--- app/src/main/java/dev/oscarreyes/rtmpstreamtest/activity/MainActivity.java
+++ app/src/main/java/dev/oscarreyes/rtmpstreamtest/activity/MainActivity.java
@@ -2,12 +2,17 @@ package dev.oscarreyes.rtmpstreamtest.activity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import dev.oscarreyes.rtmpstreamtest.R;
 
 public class MainActivity extends BaseActivity {
+	private boolean isStreaming = false;
 
-	private final String[] PERMISSIONS = new String[] {
+	private ImageButton streamButton;
+
+	private final String[] PERMISSIONS = new String[]{
 		Manifest.permission.RECORD_AUDIO
 	};
 
@@ -15,6 +20,8 @@ public class MainActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		this.streamButton = this.findViewById(R.id.stream_button);
 	}
 
 	@Override
@@ -32,5 +39,15 @@ public class MainActivity extends BaseActivity {
 	@Override
 	protected void onPermissionsDenied() {
 		super.onPermissionsDenied();
+	}
+
+	public void onButtonClick(View view) {
+		this.isStreaming = !this.isStreaming;
+
+		if (this.isStreaming) {
+			this.streamButton.setBackgroundResource(R.drawable.ic_stop);
+		} else {
+			this.streamButton.setBackgroundResource(R.drawable.ic_play_arrow);
+		}
 	}
 }
